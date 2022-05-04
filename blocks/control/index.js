dc92708f7de3978c18e704fbb3bf2d12c1ccafed
Simon Guest <simon@code.org>
2022-05-04 09:32:12 -0700

    Refactored imports

---
+++ blocks/control/index.js
@@ -0,0 +1,6 @@
+import {clip} from "./clip.js";
+import {playNote} from "./playNote.js";
+import {playSample} from "./playSample.js";
+import {sleep} from "./sleep.js";
+
+export {clip, playNote, playSample, sleep};
---
+++ blocks/percussion/index.js
@@ -0,0 +1,5 @@
+import {snare} from "./snare.js";
+import {kick} from "./kick.js";
+import {hihat} from "./hihat.js";
+
+export {snare, kick, hihat}
