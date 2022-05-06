1b7526d21ff3ba84b38db79fb0d82d02291e6b95
Simon Guest <simon@code.org>
2022-05-05 18:50:13 -0700

    Added bounce loop

--- blocks/looping/techno.js
+++ blocks/looping/techno.js
@@ -2,7 +2,7 @@ export let techno = {
     init: function() {
         this.appendDummyInput()
             .appendField("Techno Loop")
-            .appendField(new Blockly.FieldDropdown([["Basic","techno"], ["Choir", "techno2"]]), "LOOPTYPE");
+            .appendField(new Blockly.FieldDropdown([["Basic","techno"], ["Choir", "techno2"], ["Bounce", "techno3"]]), "LOOPTYPE");
         this.setOutput(true, "String");
         this.setColour(230);
         this.setTooltip("");
