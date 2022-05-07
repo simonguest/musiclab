564c787bd60adf65227d5f0d44fba926e08228de
Simon Guest <simon@code.org>
2022-05-06 22:05:56 -0700

    Added two more techno tracks

--- blocks/looping/techno.js
+++ blocks/looping/techno.js
@@ -2,7 +2,7 @@ export let techno = {
     init: function() {
         this.appendDummyInput()
             .appendField("Techno Loop")
-            .appendField(new Blockly.FieldDropdown([["Basic","techno"], ["Choir", "techno2"], ["Bounce", "techno3"]]), "LOOPTYPE");
+            .appendField(new Blockly.FieldDropdown([["Basic (113bpm)","techno"], ["Choir (115bpm)", "techno2"], ["Bounce (150bpm)", "techno3"], ["Moogley (140bpm)", "techno4"], ["Ivory (150bpm)", "techno5"]]), "LOOPTYPE");
         this.setOutput(true, "String");
         this.setColour(230);
         this.setTooltip("");
